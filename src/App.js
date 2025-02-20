fd003997a8f2443ee3786c9644d44c8221b44bd3
PSankhla09 <f20240451@pilani.bits-pilani.ac.in>
2025-02-20 22:11:22 +0530

    Changes done

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ import Info from "./Info/Info.js";
 import AS from "./Info/AS.js";
 import AlreadySubmitted from "./Info/AlreadySubmitted.js";
 
-
 function App() {
   return (
     <div className="App">
       <Loader />
-      
+
       <Routes>
-        <Route path="/" element={<Info />} />
+        <Route path="/Merchandise_Form" element={<Info />} />
         <Route path="/success" element={<AS />} />
         <Route path="/already-submitted" element={<AlreadySubmitted />} />
       </Routes>
-      
     </div>
   );
 }
